31aa10ecd9a7fb5bf50991f6025434b2255f3cdf
jli198 <jli198@stevens.edu>
2022-02-08 22:35:56 -0500

    file reorg + update arraysPointersHeap.cc

--- arraysPointersHeap/pointers.cc
+++ arraysPointersHeap/pointers.cc
@@ -8,6 +8,7 @@ int main() {
 
   //1. we're going to create new kind of variable
   int *pal; //nothing in here at this point in time
+  float *pipal;
 
   cout << "Value of x " << x << endl;
   cout << "Address of variable: " << &x << endl;
@@ -15,13 +16,17 @@ int main() {
 
   //2. pal to "point" to x
   pal = &x; //pal now has addr of x. It is essentially "pointing" to x
-  
+  pipal = &pi;
+
   //3. going to get pal to go change value of x
   // let's say that wanted to add 10 to value of x
   *pal = *pal + 10; // x = x + 10
 
   //final act: let's go see x
   cout << "Value of x now: " << x << endl;
+
+  pal = &y;
+  *pal = *pal + 10; //y = y + 10
 
   return 0;
 }
