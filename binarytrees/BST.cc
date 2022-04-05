8470cdf9be840f82a57545e5beae4b201e12e326
jli198 <jli198@stevens.edu>
2022-04-05 11:49:13 -0400

    addValue functiion added

--- binarytrees/BST.cc
+++ binarytrees/BST.cc
@@ -23,10 +23,56 @@ public:
 
   //1. Add values
   void addValue(int key) {
+    //check to see if tree is empty
+    //if it is, then make this value root of tree
+    if(root == NULL) {
+      root = new TreeChunk;
+      root->value = key;
+      return;
+    }
+
+    //if you are here, your tree is not empty
+    //which means have to go 'searching' for this value (key)
+    //if we fall off tree in process, we have found right position!
+    TreeChunk *fast, *slow;
+    fast = slow = root;
+
+    while(fast != NULL) {
+      slow = fast; // set this to fast and let fast go ahead (left or right)
+
+      if(fast->value == key) {
+        //duplicate
+        cout << "Sorry, not adding duplicates" << endl;
+        return;
+      }
+      else if(key < fast->value) {
+        //go left
+        fast = fast->left;
+      }
+      else {
+        //go right
+        fast = fast->right;
+      }
+    }
+
+    //now check to see if fast is NULL
+    //that's godo news: we have found spot to add 'key'
+    //slow will have address of new 'parent' to be
+
+    if(fast == NULL) {
+      TreeChunk *temp = new TreeChunk;
+      temp->value = key;
+
+      if(key < slow->value) {
+        slow->left = temp;
+      }
+      else
+        slow->right = temp;
+    }
 
   }
   //2. Search for value (key)
-  void searchBST(int key, TreeChunk *start) { // NOT TreeNode
+  void searchBST(int key, TreeChunk *start) {
     if(start == NULL) {
       cout << "Value not found in this tree" << endl;
     }
@@ -50,5 +96,39 @@ public:
 };
 
 int main() {
+  int choice, value;
   BST tree;
+  //jumpstart tree with values (like in slides)
+  //30, 15, 60, 7, 22, 45, 17, 27, 75
+  tree.addValue(30);
+  tree.addValue(15);
+  tree.addValue(60);
+  tree.addValue(7);
+  tree.addValue(22);
+  tree.addValue(45);
+  tree.addValue(17);
+  tree.addValue(27);
+  tree.addValue(75);
+  
+  while(1) {
+    cout << "Press 1 to add new value" << endl;
+    cout << "Press 2 to search" << endl;
+    cout << "(other options coming soon)" << endl;
+    cin >> choice;
+
+    switch(choice) {
+      case 1: cout << "What value did you want to add?" << endl;
+              cin >> value;
+              tree.addValue(value);
+              break;
+
+      case 2: cout << "Search for what?" << endl;
+              cin >> value;
+              tree.searchBST(value, tree.root);
+              break;
+
+      default: cout << "Goodbye!" << endl;
+              exit(1);
+    }
+  }
 }
